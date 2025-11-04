0898bf6a40ebfa92730712c3590bcac1d23c7e8f
SH_note <sanmhyo@gmail.com>
2025-11-04 13:39:50 +0900

    공에 맞으면 크기가 반으로 줄어듬

--- zombie.py
+++ zombie.py
@@ -33,6 +33,7 @@ class Zombie:
         self.load_images()
         self.frame = random.randint(0, 9)
         self.dir = random.choice([-1,1])
+        self.size = 200
 
 
     def get_bb(self):
@@ -51,15 +52,16 @@ class Zombie:
 
     def draw(self):
         if self.dir < 0:
-            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, self.size, self.size)
         else:
-            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, self.size, self.size)
         draw_rectangle(*self.get_bb())
 
     def handle_event(self, event):
         pass
 
     def handle_collision(self, group, other):
+
         if group == 'zombie:ball':
-            game_world.remove_object(self)
+            self.size = 100
 
